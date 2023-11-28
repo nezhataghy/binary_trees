4e1ba12de91f848297071921910cfe91cebb7e45
nezhataghy <nezhataghy@gmail.com>
2023-11-28 20:24:47 +0000

    Delete

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,23 +9,23 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *n_noeud;
+	binary_tree_t *n_noeud;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    n_noeud = binary_tree_node(parent, value);
-    if (n_noeud == NULL)
-        return (NULL);
+	n_noeud = binary_tree_node(parent, value);
+	if (n_noeud == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        n_noeud->right = parent->right;
-        parent->right = n_noeud;
-        n_noeud->right->parent = n_noeud;
-    }
-    else
-        parent->right = n_noeud;
+	if (parent->right != NULL)
+	{
+		n_noeud->right = parent->right;
+		parent->right = n_noeud;
+		n_noeud->right->parent = n_noeud;
+	}
+	else
+		parent->right = n_noeud;
 
-    return (n_noeud);
+	return (n_noeud);
 }
