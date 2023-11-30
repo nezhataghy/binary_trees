9408685f65b36e277d4e61d3cb010fcf02a11cc7
nezhataghy <nezhataghy@gmail.com>
2023-11-30 21:02:13 +0000

    Is it a perfect tree? (betty fixed)

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -33,7 +33,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
