315266fb3cb840fa6959d42e718d66523a9ead83
nezhataghy <nezhataghy@gmail.com>
2023-11-30 20:55:53 +0000

    test

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,10 +30,11 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	else if (!tree->left || !tree->right)
 		return (0);
-	
+
 	if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right))
 			return (1);
 	}
 	return (0);
