95903f3a8e03a5fe187ee04ff5b9b0a6a4d8a340
nezhataghy <nezhataghy@gmail.com>
2023-11-30 21:00:47 +0000

    Is it a perfect tree?

---
+++ 16-binary_tree_is_perfect.c
@@ -0,0 +1,40 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: pointer to the root node of the tree to count the nodes in
+ * Return: number of nodes with at least 1 child in a binary tree
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t gauche, droite;
+
+    if (!tree || (tree && !tree->left && !tree->right))
+        return (0);
+    gauche = binary_tree_nodes(tree->left);
+    droite = binary_tree_nodes(tree->right);
+    return (1 + gauche + droite);
+}
+
+/**
+ * binary_tree_is_perfect - checks if a binary tree is perfect
+ * @tree: pointer to the root node of the tree to check
+ * Return: 1 if perfect, 0 otherwise
+ * If tree is NULL, return 0
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+    if (!tree)
+        return (0);
+    if (!tree->left && !tree->right)
+        return (1);
+    else if (!tree->left || !tree->right)
+        return (0);
+
+    if (binary_tree_nodes(tree->left) == binary_tree_nodes(tree->right))
+    {
+        if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+            return (1);
+    }
+    return (0);
+}
