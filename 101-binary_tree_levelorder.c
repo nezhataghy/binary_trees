ad12ab1c42ef03f6f82bf60216f8cd775ba69c50
nezhataghy <nezhataghy@gmail.com>
2023-12-02 00:42:29 +0000

    Level-order traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -6,17 +6,18 @@
  */
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 {
-	if (!tree || !func)
-		return;
-	if (!tree->parent)
+	if (tree && func)
 	{
-		func(tree->n);
-	}
-	if (tree->left)
-		func(tree->left->n);
-	if (tree->right)
-		func(tree->right->n);
-	binary_tree_levelorder(tree->left, func);
+		if (tree->parent == NULL)
+		{
+			func(tree->n);
+		}
+		if (tree->left)
+			func(tree->left->n);
+		if (tree->right)
+			func(tree->right->n);
 
-	binary_tree_levelorder(tree->right, func);
+		binary_tree_levelorder(tree->left, func);
+		binary_tree_levelorder(tree->right, func);
+	}
 }
